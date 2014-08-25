4e11ca40d47b0b1762d5dd835234b1923f1948cf
Chris Packham <judge.packham@gmail.com>
2014-08-26 10:40:32 +1200

    Update packaging

--- setup.py
+++ setup.py
@@ -3,4 +3,6 @@ setup(name='imap-am',
       version='1.0',
       description='Apply git patches from an IMAP folder',
       author='Chris Packham',
+      author_email='judge.packham@gmail.com',
+      url='http://github.com/cpackham/imap-am',
       scripts=['imap-am'])
